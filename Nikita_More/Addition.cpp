a9db584ed1189f52f6ed78977eb02ee982b23790
morenikita1105 <nikitamore1105@gmail.com>
2025-07-11 12:13:47 +0530

    Create Addition.cpp

---
+++ Nikita_More/Addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+    int a,b,c;
+    std::cout<<"Enter the value of a : ";
+    std::cin>>a;
+    std::cout<<"Enter the value of b : ";
+    std::cin>>b;
+    c=a+b;
+    std::cout<<"The sum of a and b is : "<<c<<std::endl;
+    return 0;
+}
